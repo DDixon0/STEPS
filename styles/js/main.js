ec31c64f56cb9fa1d7d8663c3a898fbcd71700ee
Student <student@Students-MacBook-Pro.local>
2018-08-13 13:54:56 -0400

    auth

--- styles/js/main.js
+++ styles/js/main.js
@@ -3,6 +3,15 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+
+// // Initialize Cloud Firestore through Firebase
+// var db = firebase.firestore();
+
 
 
 

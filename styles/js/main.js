4b6528adcaed39dc4f394a2298dd9c6877e375b9
Student <student@Students-MacBook-Pro.local>
2018-08-13 13:38:28 -0400

    auth

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,4 +1,13 @@
+
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+
+
+
+
 const auth = firebase.auth();
 auth.signInWithEmailAndPassword(email, pass);
 auth.createUserWithEmailAndPassword(email, pass);
-auth.onAuthStateChanged(firebaseUser => { });
+auth.onAuthStateChanged(firebaseUser => { }); 

2ac15b64dc4bc725f98b1e1453e4d95949758c3c
Student <student@BAM-035068-SC.mlbam.mlb.org>
2018-08-14 11:08:06 -0400

    fix htnl

--- styles/js/main.js
+++ styles/js/main.js
@@ -36,42 +36,6 @@ btnLogin.addEventListener("click", e => {
 	});
 });
 
-// const btnSignUp = document.getElementById("btnSignUp");
-// const btnLogout = document.getElementById("btnLogout");
-
-// btnSignUp.addEventListener("click", e => {
-
-// 	e.preventDefault();
-
-// 	const credentials = getCredentials()
-// 	const email = credentials.email;
-// 	const password = credentials.password;
-	
-// 	firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
-// 		console.log('success');
-// 	}).catch(function(error) {
-// 	  // Handle Errors here.
-// 	  var errorCode = error.code;
-// 	  var errorMessage = error.message;
-// 	  console.log(errorMessage);
-// 	  // ...
-// 	});
-// });
-
-
-
-// btnLogout.addEventListener("click", e => {
-// 	e.preventDefault();
-// 	firebase.auth().signOut().then(function() {
-// 	  // Sign-out successful.
-// 	  console.log('success');
-// 	}).catch(function(error) {
-// 	  // An error happened.
-// 	  console.log(error);
-// 	});
-
-// });
-
 
 
 

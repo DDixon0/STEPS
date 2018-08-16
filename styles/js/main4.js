ddca504ba1eeaad382f7e2aa5db6774fed59c4d3
Student <student@Students-MacBook-Pro.local>
2018-08-16 15:22:56 -0400

    update

--- styles/js/main4.js
+++ styles/js/main4.js
@@ -11,3 +11,38 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
         console.log("Not logged in. or not working");
     }
 });
+
+
+
+
+const database = firebase.database();
+
+
+//When you log in
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+	if(firebaseUser){
+        getName(firebaseUser.uid);
+    } else{
+        console.log("Not logged in. or not working");
+    }
+});
+
+// .then(function(response){
+// 	firstname();
+// });
+
+
+
+function getName(uid){
+	database.ref('users/' + uid).once('value').then(function(snapshot) {
+		if(snapshot.exists()){
+			const ready = document.getElementById("notready");
+			const name = snapshot.val()['firstname'];
+			ready.innerText = "Not just ready yet..." + name;
+		} else{
+			console.log("NO name");
+		}
+	})
+	
+};
